8feeff32146e98ba95a8d01ba1a1e1282dc5d8f7
Dorian Diaconu <doriandiaconu@yahoo.com>
2015-02-12 18:29:59 +0200

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,4 +6,4 @@ TARGET_KERNEL_SOURCE := kernel/samsung/galaxys2plus-common
 TARGET_KERNEL_CONFIG := cyanogenmod_s2ve_defconfig
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/i9105/ramdisk/fstab.capri_ss_s2ve
+TARGET_RECOVERY_FSTAB := device/samsung/s2ve/ramdisk/fstab.capri_ss_s2ve
