f05a2e5cc575157973e904d88364af2fcdf0f4c4
Dorian Diaconu <doriandiaconu@yahoo.com>
2015-02-12 18:31:44 +0200

    Update and rename full_i9105.mk to full_s2ve.mk

--- full_s2ve.mk
+++ full_s2ve.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, device/samsung/i9105/i9105.mk)
+$(call inherit-product, device/samsung/s2ve/s2ve.mk)
 
-PRODUCT_DEVICE := i9105
-PRODUCT_NAME := full_i9105
+PRODUCT_DEVICE := s2ve
+PRODUCT_NAME := full_s2ve
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9105
